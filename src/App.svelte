af636042050690ba851d07766cbe79a5c5841988
handy <hk@gaudiolab.com>
2023-08-09 18:21:00 +0900

    init : 기본 파일 업로드 재생, 중지 기능 추가

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,61 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  let audioBuffer: AudioBuffer | null = null; // 로드된 AudioBuffer를 저장할 변수
+  let audioContext: AudioContext | null = null; // AudioContext를 저장할 변수
+  let isPlaying = false; // 오디오 재생 상태를 저장할 변수
+
+  async function handleFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const file = input.files && input.files[0];
+
+    if (file) {
+      const arrayBuffer = await file.arrayBuffer();
+      audioContext = new AudioContext();
+
+      audioContext.decodeAudioData(arrayBuffer, (decodedBuffer) => {
+        audioBuffer = decodedBuffer;
+      });
+    }
+  }
+
+  function playAudio() {
+    if (audioBuffer && audioContext) {
+      const source = audioContext.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(audioContext.destination);
+      source.start(0);
+      isPlaying = true;
+    }
+  }
+
+  function stopAudio() {
+    if (audioContext) {
+      audioContext.close();
+      audioContext = null;
+      isPlaying = false;
+    }
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>로컬 오디오 플레이어</h1>
+  <input type="file" accept="audio/*" on:change={handleFileChange} />
+  {#if audioBuffer}
+    <button on:click={playAudio}>재생</button>
+    <button on:click={stopAudio}>정지</button>
+  {/if}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  main {
+    text-align: center;
+    padding: 20px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  h1 {
+    margin-bottom: 10px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  input {
+    margin-bottom: 10px;
   }
 </style>
